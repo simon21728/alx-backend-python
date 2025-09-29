a78448a259672f52dddb1625758a835fcfa3a623
Sewmehon Bayu <simonbayu2211@gmail.com>
2025-09-29 11:05:46 -0300

    Implement Signals for User Notifications

--- Django_signals_orm_0x04/messaging/models.py
+++ Django_signals_orm_0x04/messaging/models.py
@@ -4,8 +4,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Get the user model
-User = get_user_model()
-
+User = get_user_model() 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
