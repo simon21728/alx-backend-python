1665bed505e52bf5dfdbecb3b3af2575d314c8f3
Simon <simonbayu2211@exgmail.com>
2025-11-19 11:07:12 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,15 +3,13 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
-
 class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map function."""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns the correct value."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
