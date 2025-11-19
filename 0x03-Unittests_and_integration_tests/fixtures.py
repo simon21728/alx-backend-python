251c1f1ae36370fad5999794e049ec417c5564d3
Simon <simonbayu2211@exgmail.com>
2025-11-19 15:24:04 +0300

    Integration test: fixtures

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Fixtures for GithubOrgClient integration tests.
+"""
+
+org_payload = {
+    "login": "google",
+    "id": 123,
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+]
+
+expected_repos = ["repo1", "repo2"]
+apache2_repos = ["repo1"]
