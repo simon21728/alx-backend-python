90dd89fad00985b29e624558213bb78434ca8a57
Simon <simonbayu2211@exgmail.com>
2025-11-19 11:41:31 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -80,7 +80,3 @@ class TestMemoize(unittest.TestCase):
             # Call property twice; should call a_method only once
             result1 = obj.a_property
             result2 = obj.a_property
-
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
