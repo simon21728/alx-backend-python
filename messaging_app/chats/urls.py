da177869c5eab1250702b7a95d1eeaf8bcf43d63
Simon <simonbayu2211@exgmail.com>
2025-11-19 10:50:43 +0300

    Set up url routing

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,18 @@
+
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
-# Create DRF router and register viewsets
+# Parent router
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
-# Include router URLs
+# Nested router for messages under conversations
+conversation_router = nested_routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,7 +1,9 @@
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('messaging_app.chats.urls')),  # Add API URLs here
+    path('api/', include('messaging_app.chats.urls')),  # API routes
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
 ]
