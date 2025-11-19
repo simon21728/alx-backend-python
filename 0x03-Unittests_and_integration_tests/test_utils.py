7b4fe877e601aa4b43207a01fe859f9f495d4b89
Simon <simonbayu2211@exgmail.com>
2025-11-19 12:05:43 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,8 +64,8 @@ class TestMemoize(unittest.TestCase):
             """Test class with a method and a memoized property."""
             def a_method(self):
                 return 42
+            
             @memoize
-
             def a_property(self):
                 return self.a_method()
         obj = TestClass()
